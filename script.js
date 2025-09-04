7c4875c87181ec86abc905522feeaed67fc63888
Kazi Foisal <108875654+kazifoisal@users.noreply.github.com>
2025-09-04 14:42:00 +0600

    Done today's task

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const startCountAnimation = (e) => {
   let start = 0;
   let end = parseInt(e.dataset.num);
   let duration = 2000;
-  
+
   let count = setInterval(() => {
     start++;
     e.textContent = start + "%";
@@ -17,14 +17,14 @@ const handleIntersection = (entries, observer) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       startCountAnimation(entry.target);
-            observer.unobserve(entry.target);
+      observer.unobserve(entry.target);
     }
   });
 };
 
 const options = {
-  root: null, 
-  threshold: 0.5 
+  root: null,
+  threshold: 0.5
 };
 
 const observer = new IntersectionObserver(handleIntersection, options);
@@ -34,3 +34,62 @@ const numElements = document.querySelectorAll(".num");
 numElements.forEach(element => {
   observer.observe(element);
 });
+
+
+
+
+
+const openModalBtn = document.querySelector(".open-modal");
+const modalWrapperElemnt = document.querySelector(".modal-wrapper");
+const modalContentElemnt = document.querySelector(".modal-content");
+const closeModalBtn = document.querySelector(".close-modal");
+
+
+openModalBtn.classList.add("open-modal");
+
+modalWrapperElemnt.classList.add("modal-wrapper");
+
+modalContentElemnt.classList.add("modal-content");
+
+
+openModalBtn.addEventListener("click", () => {
+  modalWrapperElemnt.style.display = "block"
+})
+
+closeModalBtn.addEventListener("click", () => {
+  modalWrapperElemnt.style.display = "none"
+})
+
+const cards = document.querySelectorAll(".card")
+
+cards.forEach(card => {
+  card.addEventListener("click", () => {
+
+  card.classList.toggle("is-flipped")
+
+    console.log(card)
+  })
+})
+
+
+
+
+
+
+document.querySelectorAll(".faq-header").forEach(header => {
+      header.addEventListener("click", () => {
+        const item = header.parentElement;
+        item.classList.toggle("active");
+      });
+    });
+
+
+  const btn = document.getElementById('toggleBtn');
+  const moreRows = document.querySelectorAll('.more');
+
+  btn.addEventListener('click', () =>{
+    moreRows.forEach(r => r.classList.toggle('hidden'));
+    btn.textContent = btn.textContent === "See More" ? "See Less" : "See More";
+  });
+
+
